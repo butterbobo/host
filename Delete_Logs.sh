89f26736bc4403e83cd20773084e8f1b124eb4c6
butterbobo <tempo7766@gmail.com>
2024-03-12 02:21:24 +0800

    Add files via upload

---
+++ Delete_Logs.sh
@@ -0,0 +1,44 @@
+echo " I'm ButterCow 🐮 "
+echo " I help you to Delete All .log Files "
+
+
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/cache/Cache/Log
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/APM
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/GEM
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/GCloudCore
+rm -rf rm -rf /storage/self/primary/Android/data/com.garena.game.codm/files/centauri/Log/com.garena.game.codm/*.txt
+rm -rf /storage/self/primary/centauri/Log/com.garena.game.codm/*.txt
+rm -rf /data/data/com.garena.game.codm/files/AFRequestCache
+rm -rf /data/data/com.garena.game.codm/files/com.gcloudsdk.gcloud.gvoice/tqos
+rm -rf /data/data/com.garena.game.codm/app_crashSight/tomb_*.txt
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/files/Disable_Smart.dat
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/files/disable_smart.dat
+rm -rf /storage/self/primary/Android/data/com.garena.game.codm/files/v.splash
+rm -rf /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/native-reports
+rm -rf /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/open-reports
+rm -rf /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/priority-reports
+rm -rf /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/reports
+rm -rf /data/data/com.garena.game.codm/files/AFExceptionsCache
+
+mkdir /storage/self/primary/Android/data/com.garena.game.codm/cache/Cache/Log
+mkdir /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/APM
+mkdir /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/GEM
+mkdir /storage/self/primary/Android/data/com.garena.game.codm/cache/GCloudSDKLog/GCloudCore
+mkdir /data/data/com.garena.game.codm/files/AFRequestCache
+mkdir /data/data/com.garena.game.codm/files/com.gcloudsdk.gcloud.gvoice/tqos
+mkdir /data/data/com.garena.game.codm/files/AFExceptionsCache
+mkdir /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/native-reports
+mkdir /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/open-reports
+mkdir /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/priority-reports
+mkdir /data/data/com.garena.game.codm/files/.com.google.firebase.crashlytics.files.v2:com.garena.game.codm/reports
+
+chmod 700 /data/data/com.garena.game.codm/files/AFRequestCache
+chmod 700 /data/data/com.garena.game.codm/files/com.gcloudsdk.gcloud.gvoice/tqos
+chmod 700 /data/data/com.garena.game.codm/files/AFExceptionsCache
+
+echo " Mission Complete "
+echo " Have Fun 😉 "
+
+
+
+
